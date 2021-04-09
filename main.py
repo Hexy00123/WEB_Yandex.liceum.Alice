9a0d1e1639e42a98178c90e4bef6dec4dde07a2d
hexy00123 <r.izmajlov2018@yandex.ru>
2021-04-09 17:35:45 +0400

    heroku test

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST'])
+def main():
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+    req = request.json
+    if req['session']['new']:
+        response['response']['text'] = 'Привет!'
+    else:
+        response['response']['text'] = 'Рада снова вас видеть здесь!'
+
+    return json.dumps(response)
