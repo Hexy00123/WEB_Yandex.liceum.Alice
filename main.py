d17a62f908da7177da21c8e8ab73500af4eb1dca
hexy00123 <r.izmajlov2018@yandex.ru>
2021-04-25 16:03:02 +0400

    dialog1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import requests
+import os
 import logging
 import json
 import random
@@ -199,4 +199,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
